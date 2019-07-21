1a86d9f425ef3e0f2a5176777148d5ec877ea832
Juan Pablo Cano <jp.cano@uniandes.edu.co>
2019-07-20 22:01:59 -0500

    Cambios menores

--- users/permissions/users.py
+++ users/permissions/users.py
@@ -9,4 +9,3 @@ class IsAccountOwner(permissions.BasePermission):
     """ Check obj and the user are the same """
     def has_object_permission(self, request, view, obj):
         return request.user == obj
-
